fc3d116fa7871cc2a6f37a117f53cb1b7af617a5
Andrey Marin <51476457+AndreyMarMar@users.noreply.github.com>
2020-02-24 19:29:49 +0300

    added homework 3

---
+++ app/src/main/java/com/example/skillboxkotlin/ListOfNum.kt
@@ -0,0 +1,68 @@
+package com.example.skillboxkotlin
+
+fun main() {
+
+    var count = 0
+
+    print("Введите число: ")
+    val n = readLine()?.toIntOrNull()
+
+    val list = listOfNumbers(n)
+
+    for (numElement in list) {
+        if (numElement < 0) continue
+        count += 1
+    }
+    val set = setOf<Int>().union(list)
+
+
+
+    println("Количество уникальных введённых чисел: ${set.size}")
+    println("Количество положительных чисел: $count")
+    println("Чётные числа среди введённых данных: ")
+
+    for (numElement in list) {
+        if (numElement % 2 == 1) continue
+        println("$numElement ")
+    }
+
+    println("Cумма введённых чисел: ${listElementsSum(list)}")
+    for (numElement in list){
+        println("Число <$numElement>, Сумма <${listElementsSum(list)}>, НОД <${calculateNod(numElement, listElementsSum(list))}>")
+    }
+
+
+}
+
+
+fun listOfNumbers(n: Int?): MutableList<Int> {
+    val list = mutableListOf<Int>()
+    val range = 1..n!!
+    for (current in range) {
+        val nums = readLine()?.toIntOrNull()
+        list.add(nums!!)
+    }
+    return list
+}
+
+fun listElementsSum(list: List<Int>) : Int {
+    var sum = 0
+    for (currentNum in list){
+        sum += currentNum
+    }
+    return sum
+}
+
+
+tailrec fun calculateNod (a: Int, b : Int) : Int {
+    return if(a != b){
+        if (a > b){
+            calculateNod(a - b, b)
+        } else{
+            calculateNod(a, b - a)
+        }
+
+    } else{
+        a
+    }
+}
--- module2/src/main/java/com/example/module2/MainActivity.kt
+++ module2/src/main/java/com/example/module2/MainActivity.kt
@@ -2,21 +2,11 @@ package com.example.module2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var textView : TextView = findViewById(R.id.tvMain)
-        textView.text = """
-            BuildType = ${BuildConfig.BUILD_TYPE}
-            Flavor = ${BuildConfig.FLAVOR}
-            VersionCode = ${BuildConfig.VERSION_CODE}
-            VersionName = ${BuildConfig.VERSION_NAME}
-            ApplicationId = ${BuildConfig.APPLICATION_ID}
-        """.trimIndent()
     }
 }
