331d542d330345c6d8b598bfe5b82be44ed3cdb4
Andrey Marin <51476457+AndreyMarMar@users.noreply.github.com>
2020-04-29 11:39:18 +0300

    add module7

--- app/src/main/java/com/example/skillboxkotlin/MainActivity.kt
+++ app/src/main/java/com/example/skillboxkotlin/MainActivity.kt
@@ -2,14 +2,8 @@ package com.example.skillboxkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
-import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.isVisible
-import com.bumptech.glide.Glide
+import androidx.appcompat.widget.SearchView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,56 +12,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Glide.with(this).load("https://i.picsum.photos/id/1026/4621/3070.jpg").into(imageView)
+        val stringWithPoem = getString(R.string.spongeBobSong)
+        val listWithPoemWords = stringWithPoem.split(" ")
 
-        loginButton.setOnClickListener {
-
-            loginButton.isEnabled = false
-            etEmail.isEnabled = false
-            etPassword.isEnabled = false
-            checkBox.isEnabled = false
-
-            val layout: ConstraintLayout = findViewById(R.id.container)
-            val set = ConstraintSet()
-            set.clone(layout)
-            val progressBar = ProgressBar(this)
-            progressBar.id = View.generateViewId()
-            layout.addView(progressBar)
-            set.connect(
-                progressBar.id,
-                ConstraintSet.BOTTOM,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.BOTTOM,
-                150
-            )
-            set.connect(
-                progressBar.id,
-                ConstraintSet.RIGHT,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.RIGHT,
-                0
-            )
-            set.connect(
-                progressBar.id,
-                ConstraintSet.LEFT,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.LEFT,
-                0
-            )
-            set.constrainHeight(progressBar.id, 150)
-            set.applyTo(layout)
-
-            Handler().postDelayed({
-                loginButton.isEnabled = true
-                etEmail.isEnabled = true
-                etPassword.isEnabled = true
-                checkBox.isEnabled = true
-                progressBar.visibility = View.GONE
-                Toast.makeText(this, "Login was successful", Toast.LENGTH_SHORT).show()
-            }, 2000)
-
+        toolbarMainAct.setNavigationOnClickListener {
+            toast("You have clicked on the Navigation Icon")
         }
 
+        toolbarMainAct.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.firstElementMainMenu -> {
+                    toast("You have clicked on item 1")
+                    true
+                }
+
+                R.id.secondElementMainMenu -> {
+                    toast("You have clicked on item 2")
+                    true
+                }
+
+                else -> {
+                    toast("You have clicked on item 3")
+                    true
+                }
+            }
+        }
+
+        val searchItem = toolbarMainAct.menu.findItem(R.id.searchMainMenu)
+        (searchItem.actionView as SearchView).setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                listWithPoemWords.filter { it.contains(newText ?: "") }
+                    .joinToString()
+                    .let { tvSecondAct.text = it }
+                return true
+            }
+        })
+
     }
+
+    fun toast(toastMessage: String) {
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+    }
+
 }
+
 
