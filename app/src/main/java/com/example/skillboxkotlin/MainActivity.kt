dd64fbdc5956d58d18762d2ac8c91f50f383a2ec
Andrey Marin <51476457+AndreyMarMar@users.noreply.github.com>
2020-05-01 09:29:07 +0300

    added image, corrected element menu id, added setOnActionExpandListener

--- app/src/main/java/com/example/skillboxkotlin/MainActivity.kt
+++ app/src/main/java/com/example/skillboxkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.skillboxkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         val listWithPoemWords = stringWithPoem.split(" ")
 
         toolbarMainAct.setNavigationOnClickListener {
-            toast("You have clicked on the Navigation Icon")
+            toast("You clicked on navigation icon")
         }
 
         toolbarMainAct.setOnMenuItemClickListener { menuItem ->
@@ -30,15 +31,26 @@ class MainActivity : AppCompatActivity() {
                     toast("You have clicked on item 2")
                     true
                 }
-
-                else -> {
-                    toast("You have clicked on item 3")
-                    true
-                }
+                else -> false
             }
         }
 
         val searchItem = toolbarMainAct.menu.findItem(R.id.searchMainMenu)
+
+        searchItem.setOnActionExpandListener(object: MenuItem.OnActionExpandListener{
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                toast("You expand the search")
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                textView.text = stringWithPoem
+                return true
+            }
+
+        })
+
+
         (searchItem.actionView as SearchView).setOnQueryTextListener(object :
             SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -46,9 +58,9 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                listWithPoemWords.filter { it.contains(newText ?: "") }
+                listWithPoemWords.filter { it.contains(newText ?: "", true) }
                     .joinToString()
-                    .let { tvSecondAct.text = it }
+                    .let { textView.text = it }
                 return true
             }
         })
