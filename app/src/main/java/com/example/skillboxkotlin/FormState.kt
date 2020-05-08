99dac214b6cad2326efbbcb706c66fb754f34862
Andrey Marin <51476457+AndreyMarMar@users.noreply.github.com>
2020-05-08 15:23:50 +0300

    added changes according to home work 8.12

---
+++ app/src/main/java/com/example/skillboxkotlin/FormState.kt
@@ -0,0 +1,49 @@
+package com.example.skillboxkotlin
+
+import android.os.Build
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.annotation.RequiresApi
+
+data class FormState(
+    val valid: Boolean, val message: String)
+: Parcelable {
+    @RequiresApi(Build.VERSION_CODES.Q)
+    constructor(parcel: Parcel) : this(
+        parcel.readBoolean(),
+        parcel.readString().orEmpty()
+    ) {
+    }
+
+    fun withError(): FormState{
+        return copy(false, "Fill out all the necessary fields, please!")
+    }
+
+    fun noError(): FormState{
+        return copy(false, "")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeBoolean(valid)
+        parcel.writeString(message)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<FormState> {
+        @RequiresApi(Build.VERSION_CODES.Q)
+        override fun createFromParcel(parcel: Parcel): FormState {
+            return FormState(parcel)
+        }
+
+        override fun newArray(size: Int): Array<FormState?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+
+
