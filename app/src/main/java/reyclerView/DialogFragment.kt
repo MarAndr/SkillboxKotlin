0866ded4c3d5dcd1d7210773abb3b9bb4c17dc66
MarAndr <anmarin86@gmail.com>
2020-08-26 20:20:17 +0300

    add logic in dialog to forbid closing dialog without text in fields

--- app/src/main/java/reyclerView/DialogFragment.kt
+++ app/src/main/java/reyclerView/DialogFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
 import com.example.skillboxkotlin.R
 import kotlinx.android.synthetic.main.dialog_dialogfragment.*
@@ -20,21 +21,22 @@ class AddInfoDialogFragment: DialogFragment() {
 
         val view = LayoutInflater.from(context).inflate(R.layout.dialog_dialogfragment, null)
 
-        return AlertDialog.Builder(context)
+        val dialog = AlertDialog.Builder(context)
             .setTitle("Hello!")
             .setMessage("Add characteristic of a new movie figure:")
             .setView(view)
             .setPositiveButton("add"){_,_ ->
-                if(view.etAge_dialogFragment.toString().isNotBlank() && view.etName_dialogFragment.toString().isNotBlank()){
                     (parentFragment as DialogButtonClick).onPositiveButtonClick(view.etName_dialogFragment.text.toString(),
                         view.etAge_dialogFragment.text.toString().toInt(), view.spinner.selectedItem.toString())
                     Toast.makeText(requireContext(), "fill required fields", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(requireContext(), "fill required fields", Toast.LENGTH_SHORT).show()
-                }
-
             }
             .setNegativeButton("cancel"){_,_ -> (parentFragment as DialogButtonClick).onNegativeButtonClick()}
             .create()
+
+        view.etAge_dialogFragment.addTextChangedListener { dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = view.etAge_dialogFragment.text.isNotBlank() }
+
+        dialog.setOnShowListener { (it as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false }
+
+        return dialog
     }
 }
