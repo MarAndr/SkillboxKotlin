a6fd456c438574295bf8f5419139675b456cdb95
MarAndr <anmarin86@gmail.com>
2020-10-29 15:22:26 +0300

    add module 17 commit

--- app/src/main/java/module17/DetailInfoFragment.kt
+++
@@ -1,7 +0,0 @@
-package module17
-
-import androidx.fragment.app.Fragment
-import com.example.skillboxkotlin.R
-
-class DetailInfoFragment: Fragment(R.layout.fragment_detailinfo) {
-}
--- app/src/main/java/module17/HostActivity.kt
+++
@@ -1,8 +0,0 @@
-package module17
-
-import androidx.appcompat.app.AppCompatActivity
-import com.example.skillboxkotlin.R
-
-class HostActivity: AppCompatActivity(R.layout.activity_host) {
-
-}
